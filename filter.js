75771ccf502869270e03970f335f7035d806cbd1
Katalin Molnar <74814196+molnarlin@users.noreply.github.com>
2024-05-08 11:56:07 +0100

    methods added

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul');
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,9 @@ const students = [
     ]
   },
 ];
+
+const has5YearsExp = skills => skills.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+
+console.log(candidates)
